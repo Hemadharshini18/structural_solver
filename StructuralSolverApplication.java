bfbabeeab984aea4fe31b24f697d3f502b3a7578
Hemadharshini <115564437+Hemadharshini18@users.noreply.github.com>
2025-11-13 12:43:58 +0530

    Add files via upload

---
+++ StructuralSolverApplication.java
@@ -0,0 +1,11 @@
+package com.example.structuralsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuralSolverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StructuralSolverApplication.class, args);
+    }
+}
